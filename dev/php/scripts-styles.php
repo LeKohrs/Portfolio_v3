9c1c9f5ddf6761c1357dd9e54bbe22ba4abf40ad
LeKohrs <ryan.leichliter@icloud.com>
2016-07-11 10:58:02 -0500

    added fonts

--- dev/php/scripts-styles.php
+++ dev/php/scripts-styles.php
@@ -11,9 +11,10 @@ if(!function_exists('abessive_scripts_method')) {
         wp_enqueue_script('jquery', '', '', '', true);
         wp_enqueue_script('abessive_modernizr', get_template_directory_uri() . '/js/vendor/modernizr.2.8.3.min.js');
         wp_enqueue_script('abessive_app', get_template_directory_uri() . '/js/site/app.js','', '', true);
+        wp_enqueue_script('abessive_app', get_template_directory_uri() . '/js/vendor/TweenMax.min.js','', '', true);
+        wp_enqueue_script('abessive_app', get_template_directory_uri() . '/js/vendor/TimelineMax.min.js','', '', true);
 
         //stylesheet includes
-        wp_enqueue_style('google_fonts', 'http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700,300)', false);
         wp_enqueue_style('abessive_main_styles', get_template_directory_uri() . '/css/styles.css', false);
     }
 }
